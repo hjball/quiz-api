8fd21d36a6d59fa369c3276ff0e69eba4dc9aa1a
Darren Hughes <darren@thegoalmachine.co.uk>
2018-12-19 16:51:28 +0000

    end of day 3

--- database/migrations/2018_12_18_104714_create_categories_table.php
+++ database/migrations/2018_12_18_104714_create_categories_table.php
@@ -13,24 +13,15 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        // Schema::create('statuses', function (Blueprint $table) {
-        //    $table->increments('id');
-        //    $table->string('status', 75);
-        //    $table->timestamp('submitted_timestamp')->useCurrent();
-        //    $table->timestamp('approved_timestamps')->nullable();
-        // });
-
         Schema::create('categories', function (Blueprint $table) {
            $table->increments('id');
            $table->string('category',50);
            $table->integer('status_id')->default(1);
            $table->integer('attempted')->default('0');
-           $table->text('image_url',150);
+           $table->text('image_url',150)->nullable;
            $table->timestamp('submitted_timestamp')->useCurrent();
            $table->timestamp('approved_timestamp')->nullable();
            
-           // link up to statuses table
-           // $table->foreign("status_id")->references("id")->on("statuses")->onDelete("cascade");
        });
 
     }
@@ -42,7 +33,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::drop('statuses');
         Schema::dropIfExists('categories');
     }
 }
--- database/migrations/2018_12_19_103156_create_quiz_actives_table.php
+++ database/migrations/2018_12_19_103156_create_quiz_actives_table.php
@@ -16,8 +16,8 @@ class CreateQuizActivesTable extends Migration
         Schema::create('quiz_actives', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('quiz_id')->unassigned;
-            $table->integer('cat_id')->unassigned;
-            $table->integer('quest_id')->unassigned;
+            $table->integer('category_id')->unassigned;
+            $table->integer('question_id')->unassigned;
             $table->text('right_answer',1);
             $table->integer('no_of_questions')->unassinged;
             $table->integer('no_of_seconds')->unassigned;
